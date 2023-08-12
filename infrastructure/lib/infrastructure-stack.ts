96856acac47b724ee9512796cc3d89f52ce531da
Jacob Danner <jacob.danner@drake.edu>
2023-08-12 17:17:34 -0500

    deployed lambda from container

---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,21 @@
+import * as cdk from 'aws-cdk-lib';
+import * as path from 'path'
+import { Construct } from 'constructs';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
+import { DockerImageCode, DockerImageFunction } from 'aws-cdk-lib/aws-lambda'
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class APIStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+      const testLambda = new DockerImageFunction(this, 'test-lambda', {
+        code: DockerImageCode.fromImageAsset(path.join(__dirname, '../../helloWorldLambda')) 
+      })
+
+      // const api = new RestApi(this, 'bank-data-pipeline-api');
+      // const test = api.root.addResource('test')
+      // test.addMethod('GET', new LambdaIntegration(testLambda))
+
+  }
+}
